1131c9a205367a83a7cf68258a695da0fc234af3
Marcelo Juca <marcelojuca@gmail.com>
2025-09-18 11:30:57 -0600

    environment setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# import os
+
+
+def main():
+    print("Hello from langchain-course!")
+    # print(os.getenv("OPENAI_API_KEY"))
+    # print(os.getenv("GOOGLE_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
