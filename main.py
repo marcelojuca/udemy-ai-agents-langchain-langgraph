766c1ac93f6fe1115166fbc9fafc8b75396a7b0e
Marcelo Juca <marcelojuca@gmail.com>
2025-09-25 11:44:10 -0600

    [TEA-9] ReAct Search Agent - added output parsing

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
-llm = ChatOpenAI(model="gpt-4.1-nano", temperature=0)
 tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
 agent = create_react_agent(
-    llm = llm, 
-    tools = tools, 
-    prompt=react_prompt
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
 )
-agent_executor = AgentExecutor(
-    agent = agent,
-    tools = tools,
-    verbose = True,
-    handle_parsing_errors=True,
-    max_iterations=5
-)
-chain = agent_executor
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
 
 def main():
     result = chain.invoke(
-        input = {
-            "input": "search for 3 job postings for an ai engineer using langchain in Calgary on LinkedIn and list their details"
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
     )
-    print(f"Result: {result}")
+    print(result)
 
 
 if __name__ == "__main__":
