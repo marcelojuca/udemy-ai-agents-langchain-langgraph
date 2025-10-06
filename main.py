df01542483a5a77158807c9fc9766ec88513de93
Marcelo Juca <marcelojuca@gmail.com>
2025-10-06 10:01:46 -0600

    python agent

--- main.py
+++ main.py
@@ -1,56 +1,43 @@
 from dotenv import load_dotenv
-<<<<<<< HEAD
-from langchain_core.prompts import PromptTemplate
+from langchain import hub
+from langchain_openai import OpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
 from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
-from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
-import os
-=======
-
-load_dotenv()
-
-# import os
->>>>>>> f9795c4 (environment setup)
 
 
 def main():
-    print("Hello from langchain-course!")
-    # print(os.getenv("OPENAI_API_KEY"))
-    # print(os.getenv("GOOGLE_API_KEY"))
-<<<<<<< HEAD
-    information = """
-    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is an international businessman and entrepreneur known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
+    print("Start...")
 
-Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he had obtained Canadian citizenship at birth through his Canadian-born mother. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
-
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left, growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
-
-Musk was the largest donor in the 2024 U.S. presidential election, and is a supporter of global far-right figures, causes, and political parties. In early 2025, he served as senior advisor to United States president Donald Trump and as the de facto head of DOGE. After a public feud with Trump, Musk left the Trump administration and announced he was creating his own political party, the America Party.
-
-Musk's political activities, views, and statements have made him a polarizing figure, especially following the COVID-19 pandemic. He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation and promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. His role in the second Trump administration attracted public backlash, particularly in response to DOGE.
-    """
-
-    summary_template = f"""
-    given the information {information} about a person I want you to create:
-    1. a short summary
-    2. two interesting facts about them
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+    instructions = """You are an agent designed to write and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    You have qrcode package installed
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question. 
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+        """
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+    tools = [PythonREPLTool()]
+    agent = create_react_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4o-mini"),
+        tools=tools,
+        prompt=prompt,
     )
-
-    llm = ChatOpenAI(model="gpt-4.1-nano", temperature=0)
-    # llm = ChatOllama(model="gemma3:270m", temperature=0)
-    # llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", temperature=0, goole_api_key=os.getenv("GOOGLE_API_KEY"))
-    chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information": information})
-    print(response.content)
-=======
-
->>>>>>> f9795c4 (environment setup)
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        handle_parsing_errors=True,
+    )
+    input = """Generate and save in current working directory five qr codes that points to the url https://www.udemy.com/course/langchain. 
+    You have qrcode package installed.
+    """
+    result = agent_executor.invoke({"input": input})
+    print(result)
 
 if __name__ == "__main__":
     main()
