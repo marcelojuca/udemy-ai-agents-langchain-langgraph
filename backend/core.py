10b39f2537546ecfa7247853257aef0b129a876d
Marcelo Juca <marcelojuca@gmail.com>
2025-10-05 19:59:32 -0600

    RAG

--- backend/core.py
+++ backend/core.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import os
 from langchain.chains.retrieval import create_retrieval_chain
 
 load_dotenv()
@@ -9,21 +10,24 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 
-INDEX_NAME = "langchain-docs-2025"
-
 # retrieval function that retrieves the documents from the vector store
 def run_llm(query: str):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    vector_store = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
+    vector_store = PineconeVectorStore(index_name=os.getenv("PINECONE_INDEX_NAME"), embedding=embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0, model="gpt-5-mini")
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
     
     retrieval_chain = create_retrieval_chain(vector_store.as_retriever(), combine_docs_chain=stuff_documents_chain)
-    return retrieval_chain.invoke({"input": query})
-
+    result = retrieval_chain.invoke({"input": query})
+    new_result = {
+        "query": result["input"],
+        "result": result["answer"],
+        "source_documents": result["context"]
+    }
+    return new_result
 
 if __name__ == "__main__":
     res = run_llm(query = "What is LangChain chain?")
-    print(res["answer"])
+    print(res["result"])
--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from langchain-course!")
+from backend.core import run_llm
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+st.header("LangChain Udemy Course - Document Assistant Bot")
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here")
+
+if "user_prompt_history" not in st.session_state:
+    st.session_state.user_prompt_history = []
+if "chat_answer_history" not in st.session_state:
+    st.session_state.chat_answer_history = []
+
+def create_sources_string(sources_urls: set[str]) -> str:
+    if not sources_urls:
+        return ""
+    sources_list = list(sources_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
 
 
-if __name__ == "__main__":
-    main()
+if prompt:
+    with st.spinner("Thinking..."):
+        # Add a breakpoint here for debugging
+        generated_response = run_llm(query=prompt)  # Set breakpoint on this line
+        sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
+
+        formatted_response = (f"{generated_response['result']} \n\n {create_sources_string(sources)}")
+
+        st.session_state.user_prompt_history.append(prompt)
+        st.session_state.chat_answer_history.append(formatted_response)
+
+if st.session_state["chat_answer_history"]:
+    for generated_response, user_query in zip(st.session_state["chat_answer_history"], st.session_state["user_prompt_history"]):
+        st.chat_message("user").write(user_query)
+        st.chat_message("assistant").write(generated_response)
