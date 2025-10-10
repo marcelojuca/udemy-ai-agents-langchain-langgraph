20288fd98372ebff0297336b4040fd48f7aaf090
mhcj <mhcj@mhcjs-MacBook-Pro.local>
2025-10-10 16:56:39 -0600

    133.retrieval_grader and grade_documents

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,5 +1,30 @@
-def test_foo() -> None:
-    assert 1 == 1
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    test_foo()
+load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[0].page_content
+    res: GradeDocuments = retrieval_grader.invoke({"document": doc_txt, "question": question})
+
+    assert docs is not None
+    assert len(docs) > 0
+    assert hasattr(docs[0], 'page_content')
+    assert res is not None
+    assert res.binary_score == "yes"
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[0].page_content
+    res: GradeDocuments = retrieval_grader.invoke({"document": doc_txt, "question": "how to make a sandwich"})
+
+    assert docs is not None
+    assert len(docs) > 0
+    assert hasattr(docs[0], 'page_content')
+    assert res is not None
+    assert res.binary_score == "no"
